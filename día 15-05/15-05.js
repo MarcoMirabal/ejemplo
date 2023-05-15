d4e327aaabca088218afa07d0eadf713ccceecd1
MarcoMirabal <marcomirabal.9@gmail.com>
2023-05-15 17:38:50 -0300

    Update 15-05.js
    java 15 05

--- día 15-05/15-05.js
+++ día 15-05/15-05.js
@@ -1,5 +1,15 @@
-function name(params){
-
-
-    
+let num = 545
+function ejemplo(params){
+let inp = document.querySelector("#entrada");
+let p = document.querySelector("#salida");
+let divImg = document.querySelector("#img");
+divImg.innerHTML = '<img src="../día 8-05/img/ciruela.jpg"  alt="error"/>';
+let valor = inp.value;
+p.innerText = valor;
+console.log(p.value);
+console.log(inp);
+console.dir(valor);
+console.log(num);
+let r = num * "hola";
+console.log(r);
 }
