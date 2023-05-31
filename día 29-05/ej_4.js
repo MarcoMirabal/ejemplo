2eda1d3adb0c5c7575cbb1b671c14e7afd25da32
MarcoMirabal <marcomirabal.9@gmail.com>
2023-05-31 15:26:21 -0300

    Create ej_4.js

---
+++ día 29-05/ej_4.js
@@ -0,0 +1,14 @@
+var letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K',
+    'E', 'T'];
+
+var DNI = 8172631;
+var division = 23;
+
+if (DNI > 0 && DNI < 99999999) {
+    var letra = DNI % division;
+    console.log("La letra correspondiente es"+ letra);
+
+}
+else {
+    console.log("se ha detectado un error al verificar su DNI. Verifique correctamente");
+}
