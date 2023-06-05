5a89999e45228fd5288a8eba1ca020f5926d530f
MarcoMirabal <marcomirabal.9@gmail.com>
2023-06-05 17:38:01 -0300

    Update ej_4.js

--- día 29-05/ej_4.js
+++ día 29-05/ej_4.js
@@ -1,18 +1,19 @@
+function Ejercicio4(){ 
 var letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K',
     'E', 'T'];
 
-var DNI = 8172631;
+var DNI = 81723423;
 var division = 23;
 
 if (DNI > 0 && DNI < 99999999) {
-    var letra = DNI % division;
-    console.log("La letra correspondiente es" + letra);
+    var posicion = DNI % division;
+    console.log("La letra correspondiente es= " + letra);
+    letra = letras[];
 
 }
 else {
     console.log("se ha detectado un error al verificar su DNI. Verifique correctamente");
 
 
-
-
+}
 }
