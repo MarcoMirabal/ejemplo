ee1f18b0f62a0c29639de17f8e6da05d4541e8ed
dominatris <132932919+dominatris@users.noreply.github.com>
2023-06-12 17:07:26 -0300

    Update ej_4.js

--- día 29-05/ej_4.js
+++ día 29-05/ej_4.js
@@ -2,14 +2,19 @@ function Ejercicio4(){
 var letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K',
     'E', 'T'];
 
-var DNI = 81723423;
-var division = 23;
+var DNI = parseInt(prompt("Ingrese su número de DNI"));
+var letra =prompt("ingresa a la letra que corresponde a tu DNI");
+var letraUsuarioMayuscula = letra.toUpperCase();
+
 
 if (DNI > 0 && DNI < 99999999) {
-    var posicion = DNI % division;
-    console.log("La letra correspondiente es= " + letra);
-    letra = letras[];
-
+    var posicion = DNI % 23;
+    if (letras[posicion] == letraUsuarioMayuscula) {
+        console.log("El número DNI y la letra son correctos");
+    }
+    else {
+        console.log("La letra que ha indicado no es correcta");
+    }
 }
 else {
     console.log("se ha detectado un error al verificar su DNI. Verifique correctamente");
